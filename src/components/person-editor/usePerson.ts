5d4edc57b175b57b89d58b593d980a9422740822
unknown <erbulkeshkumawat@gmail.com>
2022-09-16 00:33:34 +0530

    React costum hook - UserPerson - comment added

--- src/components/person-editor/usePerson.ts
+++ src/components/person-editor/usePerson.ts
@@ -1,26 +1,37 @@
-import {useState, useEffect} from 'react';
-import localforage from 'localforage';
-import type { Person } from '../../types/person';
+import { useState, useEffect } from "react";
+import localforage from "localforage";
+import type { Person } from "../../types/person";
 
-function saveperson(person:Person | null):void{
-    console.log("Saving person : " , person);
-    localforage.setItem("person", person);
+function saveperson(person: Person | null): void {
+  console.log("Saving person : ", person);
+  localforage.setItem("person", person);
 }
 
-export function usePerson(initialPerson:Person){
-    const [person, setPerson] = useState<Person | null>(null);
+/*
+custom hook can also be exported as anonymous function
+ export default function (initialPerson:Person)
+ and can be imported in any component and assigned to any name
+ i.e => import  useMyPerson from "./usePerson";
+ const [person, setPerson] = useMyPerson(initialPerson);4
+ Or
+ Costum hook name should start with 'use' keyword followed by anysenseble name
+ i.e usePerson
+ */
 
-    useEffect(() => {
-      const getPerson = async ()=> {
-          const person = await localforage.getItem<Person>("person")
-          setPerson(person ?? initialPerson);
-      }
-      getPerson();
-    },[initialPerson]);
-  
-    useEffect(() => {
-      saveperson(person);
-    },  [person]);
+export function usePerson(initialPerson: Person) {
+  const [person, setPerson] = useState<Person | null>(null);
 
-    return [person, setPerson] as const;
+  useEffect(() => {
+    const getPerson = async () => {
+      const person = await localforage.getItem<Person>("person");
+      setPerson(person ?? initialPerson);
+    };
+    getPerson();
+  }, [initialPerson]);
+
+  useEffect(() => {
+    saveperson(person);
+  }, [person]);
+
+  return [person, setPerson] as const;
 }
