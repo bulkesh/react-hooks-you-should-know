766efd59290cd10d65cd9079891723691d93b489
unknown <erbulkeshkumawat@gmail.com>
2022-09-16 00:14:13 +0530

    React costum hook - UserPerson

---
+++ src/components/person-editor/usePerson.ts
@@ -0,0 +1,26 @@
+import {useState, useEffect} from 'react';
+import localforage from 'localforage';
+import type { Person } from '../../types/person';
+
+function saveperson(person:Person | null):void{
+    console.log("Saving person : " , person);
+    localforage.setItem("person", person);
+}
+
+export function usePerson(initialPerson:Person){
+    const [person, setPerson] = useState<Person | null>(null);
+
+    useEffect(() => {
+      const getPerson = async ()=> {
+          const person = await localforage.getItem<Person>("person")
+          setPerson(person ?? initialPerson);
+      }
+      getPerson();
+    },[initialPerson]);
+  
+    useEffect(() => {
+      saveperson(person);
+    },  [person]);
+
+    return [person, setPerson] as const;
+}
